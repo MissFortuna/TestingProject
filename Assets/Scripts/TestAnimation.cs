3d63f8c8c232544bd2d0acca613d7cc0be209b15
MissFortuna <elizabeth.lemeshko@gmail.com>
2017-09-19 18:22:56 +0200

    Import fisrt animation

---
+++ Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using DragonBones;
+
+public class TestAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Load Data	
+        UnityFactory.factory.LoadDragonBonesData("testAnimation/Rabbit_ske");
+        UnityFactory.factory.LoadTextureAtlasData("testAnimation/Rabbit_tex");
+        //Create armature
+
+        UnityArmatureComponent armatureComponent = UnityFactory.factory.BuildArmatureComponent("Armature");
+       // UnityArmatureComponent armatureComponent = GetComponent<UnityArmatureComponent>;
+        if (armatureComponent == null)
+        {
+            Debug.Log("BadNews");
+        }
+        armatureComponent.animation.Play("jump");
+    
+        armatureComponent.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+}
